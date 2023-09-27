057ee2f17f3c81f2de0578fa27407bc6e53c488c
Juan Guilherme <141837361+Gui-Muniz@users.noreply.github.com>
2023-09-27 10:22:21 -0300

    Update teorema.c

--- teorema.c
+++ teorema.c
@@ -4,9 +4,9 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int n1 = //colocar a formula aqui.
+	int v[4]n1 = //colocar a formula aqui(provavelmente usando laço de repetição aqui dentro).
 	printf("Todo inteiro par maior que 2 pode ser escrito como a soma de 2 numeros primos.\n");
-	printf("de acordo com esse teorema, digite quatro numeros inteiros maiores que 2, e o programa ira mostrar uma soma de numeros primos que correspondera a esse mesmo n�mero.\n ");
+	printf("de acordo com esse teorema, digite quatro numeros inteiros maiores que 2 que seja unidade, e o programa ira mostrar uma soma de numeros primos que correspondera a esse mesmo número.\n ");
 	scanf("%i", &n1);
 	return 0;
 }
