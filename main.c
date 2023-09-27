ccb34db6f6fba105ee268c422ab9309526f4057c
Juan Guilherme <141837361+Gui-Muniz@users.noreply.github.com>
2023-09-27 10:02:38 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int n1
+	printf("Todo inteiro par maior que 2 pode ser escrito como a soma de 2 numeros primos.\n");
+	printf("de acordo com esse teorema, digite quatro numeros inteiros maiores que 2, e o programa ira mostrar uma soma de numeros primos que correspondera a esse mesmo número.\n ");
+	scanf()
+	return 0;
+}
